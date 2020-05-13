95483919ef1278665404c439da483d82f4a7112c
wallisonmoura <wallison_cedaspy@hotmail.com>
2020-05-13 20:50:02 -0300

    Config React Navigation & StatusBar

---
+++ src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Main = () => {
+  return <View />;
+};
+
+Main.navigationOptions = {
+  headerTitleAlign: 'center',
+  title: 'Usuários',
+};
+
+export default Main;
